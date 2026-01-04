030ca2d15498be8e9bccaeee4f11d5ee41c28f8e
Juancho <juanjqogm@gmail.com>
2026-01-04 16:22:24 +0000

    Updated the headers to include the copyright.

--- include/vfi_configuration_file_yaml.hpp
+++ include/vfi_configuration_file_yaml.hpp
@@ -1,3 +1,26 @@
+/*
+#    Copyright (c) 2024-2026 Adorno-Lab
+#
+#    robot_constraint_editor is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    robot_constraint_editor is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with robot_constraint_editor.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ################################################################
+#
+#   Author: Juan Jose Quiroz Omana (email: juanjose.quirozomana@manchester.ac.uk)
+#
+# ################################################################
+*/
+
 #pragma once
 #include <memory>
 #include <vector>
