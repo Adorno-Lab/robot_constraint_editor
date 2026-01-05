197279ae5e9d43c0679fe714d2124329153cbeb1
Juancho <juanjqogm@gmail.com>
2026-01-05 20:04:24 +0000

    Added a test example that requires the rce library installed.

---
+++ examples/tests/tests.cpp
@@ -0,0 +1,56 @@
+
+#include <dqrobotics_extensions/robot_constraint_editor/vfi_configuration_file_yaml.hpp>
+#include <dqrobotics_extensions/robot_constraint_editor/robot_constraint_editor.hpp>
+#include <dqrobotics_extensions/robot_constraint_editor/utils.hpp>
+using namespace DQ_robotics_extensions;
+
+
+
+int main()
+{
+    auto ri = VFIConfigurationFileYaml("config_file.yaml");
+    VFIConfigurationFileRawData::show_data(ri.get_raw_data(), ri.get_vfi_file_version(), ri.get_zero_indexed_status());
+
+    //-- Edit the YAML file-----
+
+    auto rce = RobotConstraintEditor();
+    rce.load_data("config_file.yaml");
+
+
+    // Add a new constraint---------
+    DQ_robotics_extensions::VFIConfigurationFile::ROBOT_TO_ROBOT_RAW_DATA data;
+    data.vfi_type = "ROBOT_TO_ROBOT";
+    data.cs_entity_one = {"entity1", "entity2"};
+    data.cs_entity_two = {"entity3", "entity4"};
+    data.entity_one_primitive_type = "LINE";
+    data.entity_two_primitive_type = "PLANE";
+    data.robot_index_one = 1;
+    data.robot_index_two = 3;
+    data.joint_index_one = 5;
+    data.joint_index_two = 6;
+    data.safe_distance = 2.3;
+    data.vfi_gain = 5.1;
+    data.direction = "RESTRICTED_ZONE";
+    data.tag = "TAG_X1";
+    rce.add_data(data);
+
+    //----Edit a constraint
+    std::vector<std::string> new_cs_entity_one = {"new_entity1", "new_entity2"};
+    std::vector<std::string> new_cs_entity_two = {"new_entity3", "new_entity4"};
+    rce.edit_data("TAG_X1", "cs_entity_one", new_cs_entity_one);
+    rce.edit_data("TAG_X1", "cs_entity_two", new_cs_entity_two);
+    rce.edit_data("TAG_X1", "entity_one_primitive_type", std::string("POINT"));
+    rce.edit_data("TAG_X1", "entity_two_primitive_type", std::string("POINT"));
+    rce.edit_data("TAG_X1", "robot_index_one", 11);
+    rce.edit_data("TAG_X1", "robot_index_two", 33);
+    rce.edit_data("TAG_X1", "joint_index_one", 55);
+    rce.edit_data("TAG_X1", "joint_index_two", 66);
+    rce.edit_data("TAG_X1", "safe_distance", 3.2);
+    rce.edit_data("TAG_X1", "vfi_gain", 1.5);
+    rce.edit_data("TAG_X1", "direction", std::string("SAFE_ZONE"));
+    rce.edit_data("TAG_X1", "tag", std::string("TAG_X2"));
+
+    rce.save_data("config_file2.yaml", 2, false);
+
+    return 0;
+}
