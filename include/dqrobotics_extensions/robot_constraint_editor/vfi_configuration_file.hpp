029be9a73b880688bae1f74ad24c270a6b88b7df
Juancho <juanjqogm@gmail.com>
2026-05-06 11:11:47 +0100

    Defined a default value for the buffer

--- include/dqrobotics_extensions/robot_constraint_editor/vfi_configuration_file.hpp
+++ include/dqrobotics_extensions/robot_constraint_editor/vfi_configuration_file.hpp
@@ -36,7 +36,7 @@ public:
     struct BASE_DATA{
         std::string vfi_type;
         double safe_distance;
-        double buffer;
+        double buffer = 0.0;  //Default value
         double vfi_gain;
         std::string direction;
         std::string tag;
